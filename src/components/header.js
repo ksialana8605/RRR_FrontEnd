339f5b4775849ecab0f52600c2a538f04fe43372
ricekeet <ninjabasketball@hotmail.com>
2022-03-13 14:18:45 -0400

    Updated Navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,43 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes, Link, Outlet } from "react-router-dom";
+import SearchButton from '../img/search.png';
 
 class Header extends React.Component{
     render () {
         return(
         <div>
-            <nav className='navBar'>
-                <ul>
+            <Navbar bg='dark' variant='dark'>
+                <Container>
+                    {/* Brand Icon */}
+                    <Navbar.Brand href='/'>Radical Random Recipes</Navbar.Brand>
+
+                    {/* Pages */}
+                    <Nav className="me-auto">
+                        <Nav.Link href='/account/login'>Login</Nav.Link>
+                        <Nav.Link href='/'>Home</Nav.Link>
+                        <Nav.Link href='/about'>About</Nav.Link>
+                        <Nav.Link href='/account/profile'>Profile</Nav.Link>
+                        <Nav.Link href='/account/recipes'>My Recipes</Nav.Link>
+                        {/* <Nav.Link href='/recipe/?'>Random Recipe</Nav.Link> */}
+                        <Nav.Link href='/search'>
+                            <img src={SearchButton} height='20' alt='Search Button'/>
+                        </Nav.Link>
+                    </Nav>
+                </Container>
+                {/* <ul>
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/about'>About</Link></li>
                     <li><Link to='/account/login'>Login</Link></li>
                     <li><Link to='/account/profile'>Profile</Link></li>
                     <li><Link to='/search'>Search</Link></li>
                     <li><Link to='/account/recipes'>My Recipes</Link></li>
-                </ul>
+                </ul> */}
                 <div className='accountBox'>
                     Welcome User!
                 </div>
-            </nav>
+            </Navbar>
 
             <Outlet/>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './styles/main.scss';
 import reportWebVitals from './reportWebVitals';
+// // Importing the Bootstrap CSS
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
